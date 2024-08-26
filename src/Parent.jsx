4a0b4d921a1eb5df9074c957324362f2e628635f
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-26 16:36:55 +0530

    react hot toast added

--- src/Parent.jsx
+++ src/Parent.jsx
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Child from "./components/Child";
+import { Toaster, toast } from "react-hot-toast";
 
 const Parent = () => {
   const [input, setInput] = useState("");
   const [todo, setTodo] = useState([]);
 
   const handleClick = () => {
+    if (input === "") {
+      toast("Empty Todo Cant be added :(");
+      return;
+    }
     setTodo((prev) => [...prev, input]);
+    toast("Todo Added :)");
     setInput("");
   };
 
   const handleDelete = (val) => {
     setTodo((prev) => prev.filter((_, idx) => idx !== val));
+    toast("Todo Deleted :)");
   };
 
   const handleEdit = (idxToUpdate, val) => {
     setTodo((prev) =>
       prev.map((todo, idx) => (idx === idxToUpdate ? val : todo))
     );
+    toast("Todo Edited :)");
   };
+
+  useEffect(() => {
+    console.log("Mount + Update")
+  })
 
   useEffect(() => {
     setTodo(JSON.parse(localStorage.getItem("todos")) || []);
@@ -30,6 +42,7 @@ const Parent = () => {
 
   return (
     <>
+      <Toaster position="top-left" reverseOrder={false} />
       <div className="parentContainer">
         <h1 className="title">TODO LIST</h1>
         <div className="inputContainer">
